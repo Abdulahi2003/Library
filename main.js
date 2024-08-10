8146228332a73779d9d681ae2ddcf0a1d3b763d6
Abdulahi <abdulahiaden444@gmail.com>
2024-08-10 19:22:15 -0400

    Store books added into myLibrary array

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read; 
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    let title = document.querySelector("#title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
 }
 
 let newBookbtn = document.querySelector("#new-book-btn");
 newBookbtn.addEventListener("click", function() {
     let newBookForm = document.querySelector("#new-book-form");
     newBookForm.style.display = "block";
+})
+
+document.querySelector("#new-book-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    addBookToLibrary();
 })
